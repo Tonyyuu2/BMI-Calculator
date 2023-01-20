ec181c2064433ec701cc89ae2ea6942b6613a0f6
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-19 16:02:08 -0800

    add units radio button

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Form from './components/Form/Form';
 function App() {
   return (
     <div className="bg-green-300 w-screen h-screen flex justify-center items-center flex-col">
-      <h1>LET'S GOOOOO</h1>
+      {/* <h1>LET'S GOOOOO</h1> */}
       <Form />
     </div>
   );
--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import FormControl from "@mui/material/FormControl";
 import {
+  FormControlLabel,
   FormHelperText,
+  FormLabel,
   Input,
   InputAdornment,
   InputLabel,
+  Radio,
+  RadioGroup,
   TextField,
 } from "@mui/material";
 
 function Form() {
-  const [unit, setUnit] = useState({
-    usUnits: true,
-    metricUnits: false,
-  });
+  const [unit, setUnit] = useState("Us Units");
+  console.log("unit :", unit);
 
   const [data, setData] = useState({
     age: "",
@@ -21,6 +23,26 @@ function Form() {
 
   return (
     <div>
+      <div className="flex justify-center">
+        <FormControl>
+          <RadioGroup row defaultValue="US Units">
+            <FormControlLabel
+              value="US Units"
+              control={<Radio />}
+              label="US Units"
+              labelPlacement="top"
+              onChange={(e) => setUnit(e.target.value)}
+            />
+            <FormControlLabel
+              value="Metric Units"
+              control={<Radio />}
+              label="Metric Units"
+              labelPlacement="top"
+              onChange={(e) => setUnit(e.target.value)}
+            />
+          </RadioGroup>
+        </FormControl>
+      </div>
       <div className="bg-white w-auto h-auto p-6 flex rounded-xl flex-col ">
         <h1 className="mb-3 flex justify-center">Add values below 😊</h1>
         <FormControl className="gap-5">
@@ -62,9 +84,11 @@ function Form() {
                 ? "Tall or short, you still a king/queen"
                 : data.height < 157
                 ? "Good luck finding love..."
-                : data.height > 21
-                ? "WOAH WOAH WOAH OLD TIMER"
-                : "let's go"
+                : data.height >= 188
+                ? "How's the weather up there?😳"
+                : data.height >= 157
+                ? "Pssh... I can take you 🤺"
+                : null
             }
           />
         </FormControl>
